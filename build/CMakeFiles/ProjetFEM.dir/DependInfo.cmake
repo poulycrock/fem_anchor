4c4d44a3d76c8610696800b487dec785fc5f70de
math <mathieu.colens@student.uclouvain.be>
2025-04-03 14:18:41 +0200

    Update CMake minimum version to 3.22

---
+++ build/CMakeFiles/ProjetFEM.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/math/fem_anchor/src/fem.c" "CMakeFiles/ProjetFEM.dir/src/fem.c.o" "gcc" "CMakeFiles/ProjetFEM.dir/src/fem.c.o.d"
+  "/home/math/fem_anchor/src/homework.c" "CMakeFiles/ProjetFEM.dir/src/homework.c.o" "gcc" "CMakeFiles/ProjetFEM.dir/src/homework.c.o.d"
+  "/home/math/fem_anchor/src/main.c" "CMakeFiles/ProjetFEM.dir/src/main.c.o" "gcc" "CMakeFiles/ProjetFEM.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ProjetFEM.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetFEM.dir/src/fem.c.o"
+  "CMakeFiles/ProjetFEM.dir/src/fem.c.o.d"
+  "CMakeFiles/ProjetFEM.dir/src/homework.c.o"
+  "CMakeFiles/ProjetFEM.dir/src/homework.c.o.d"
+  "CMakeFiles/ProjetFEM.dir/src/main.c.o"
+  "CMakeFiles/ProjetFEM.dir/src/main.c.o.d"
+  "ProjetFEM"
+  "ProjetFEM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjetFEM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
